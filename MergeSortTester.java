5eba14f67fade1dba9bd53dfa566c393fa11b9f9
awang18 <awang14@stuy.edu>
2017-02-14 11:00:55 -0500

    Added distillation for algo

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -2,7 +2,15 @@
   class MergeSortTester
 
   ALGORITHM:
-  <INSERT YOUR DISTILLATION OF ALGO HERE>
+  Taking advantage of the fact that an element with one array is always sorted,
+  MergeSort utilizes recursion to sort an inputed array. The algorithm contintually
+  checks if an array has only 1 item--if true, then that array is returned; if not, 
+  the array is split into 2 smaller arrays, and the algorithm uses recursion to 
+  continue dividing the arrays until only arrays of length 1 remain. After that, 
+  the arrays are continually merged from pairs and sorted in ascending order until
+  all the arrays have been remerged and sorted into a single array of the same length
+  as the original input.
+ 
 
   BIG-OH CLASSIFICATION OF ALGORITHM:
   <INSERT YOUR EXECUTION TIME CATEGORIZATION OF MERGESORT HERE>
